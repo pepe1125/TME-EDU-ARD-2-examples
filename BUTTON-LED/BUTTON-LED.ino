64ab7b74e81978dd2379402078c74b8cba98a81e
Peter Sandor <pepe1125@gmail.com>
2022-01-01 16:45:31 +0100

    Add files via upload

--- BUTTON-LED/BUTTON-LED.ino
+++ BUTTON-LED/BUTTON-LED.ino
@@ -1,9 +1,9 @@
 /*  TME EDUCATION
- *  Buttons-LED
- *  v0.1.0
- *  -------------
- *  In this program, we will combine the skill of operating the led strip with the buttons located on the TME board
- */ 
+    Buttons-LED
+    v0.1.0
+    -------------
+    In this program, we will combine the skill of operating the led strip with the buttons located on the TME board
+*/
 #define PROJECT_NAME "Buttons-LED"
 #define VERSION "v0.1.0"
 
@@ -22,21 +22,21 @@
 ////////DECLARATIONS////////
 //This section prepares variables that are responsible for the individual components on the board
 hd44780_I2Cexp lcd(0x20, I2Cexp_MCP23008, 7, 6, 5, 4, 3, 2, 1, HIGH);
-Adafruit_NeoPixel ledStrip = Adafruit_NeoPixel(5, 12, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel ledStrip = Adafruit_NeoPixel(60, 12, NEO_GRB + NEO_KHZ800);
 
-//We want the program to enable us to turn off the LEDs, 
+//We want the program to enable us to turn off the LEDs,
 //change their color and brightness, therefore we declare below three variables
 boolean show = true; // on/off
-int value = 100;     // brightness
-int color = 100;     // color
+uint16_t value = 100;     // brightness
+uint32_t  color = 0;     // color
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
   intro(lcd);
 
-  //The buttons are connected to the appropriate pins defined above in the "CONSTANTS" section. 
-  //If the button is pressed, the signal comes to the Arduino, so we have to pass on to our program 
+  //The buttons are connected to the appropriate pins defined above in the "CONSTANTS" section.
+  //If the button is pressed, the signal comes to the Arduino, so we have to pass on to our program
   //at first that we should treat the pins as inputs. We do this using the pinMode() method
   pinMode(KEY_RIGHT , INPUT);
   pinMode(KEY_LEFT , INPUT);
@@ -47,49 +47,51 @@ void setup(){
   ledStrip.begin();
 }
 
-void loop(){
+void loop() {
   delay(10);
-  //To read whether a given button is pressed, read the value of a given pin using the digitalRea() method. 
+  //To read whether a given button is pressed, read the value of a given pin using the digitalRea() method.
   //After pressing one of the buttons, the appropriate value from the three previously declared variables changes.
 
   ////////////////////////////////////////////////////  ON / OFF
-  if(digitalRead(KEY_CENTER)){
+  if (digitalRead(KEY_CENTER)) {
     show = !show;
     //check what happens if you delete the line with a delay method below.
     //The explanation can be found in the file "explaination.png", which is located in the folder with the project
     delay(500);
   }
   ////////////////////////////////////////////////////  BRIGHTNESS ++
-  if(digitalRead(KEY_UP) && value < 255){
+  if (digitalRead(KEY_UP) && value < 255) {
     value++;
   }
   ////////////////////////////////////////////////////  BRIGHTNESS --
-  if(digitalRead(KEY_DOWN) && value > 0){
+  if (digitalRead(KEY_DOWN) && value > 0) {
     value--;
   }
   ////////////////////////////////////////////////////  COLOR ++
-  if(digitalRead(KEY_LEFT) && color < 255 ){
-    color++;
+  if (digitalRead(KEY_RIGHT)) {
+    color += 100;
+    if (color > 65500) color = 0;
+    lcd.clear();
+    lcd.print(color);
   }
   ////////////////////////////////////////////////////  COLOR --
-  if(digitalRead(KEY_RIGHT) && color > 0){
-    color--;
+  if (digitalRead(KEY_LEFT)) {
+    color -= 100;
+    if (color > 65500) color = 65500;
+    lcd.clear();
+    lcd.print(color);
   }
 
-  //At the very end of the loop, after changing the value of any of the variables, we set the diode parameters to current. 
-  //You may have noticed that to turn off the diode, I used a method that reduces its brightness to zero.  
-  for (int i = 0; i < 5 ; i++){
-  ledStrip.setPixelColor(i, 50, color, 100);
-  ledStrip.setBrightness(value);
-  if(!show){
-    ledStrip.setBrightness(0);
+  //At the very end of the loop, after changing the value of any of the variables, we set the diode parameters to current.
+  //You may have noticed that to turn off the diode, I used a method that reduces its brightness to zero.
+  uint32_t rgbcolor = ledStrip.ColorHSV(color);
+
+  for (int i = 0; i < 60 ; i++) {
+    ledStrip.setPixelColor(i, rgbcolor);
+    ledStrip.setBrightness(value);
+    if (!show) {
+      ledStrip.setBrightness(0);
+    }
   }
   ledStrip.show();
 }
-}
-
-
-
-
-
-
